591261ce1a2b8bacc879a715882359ab0a86d2e9
sujithrabkn <119477857+sujithrabkn@users.noreply.github.com>
2024-10-03 15:47:22 +0530

    Update SMOOTHING FILTERS.py

--- SMOOTHING FILTERS.py
+++ SMOOTHING FILTERS.py
@@ -1,6 +1,6 @@
 
-###Developed By : 
-###Register Number: 
+###Developed By : SUJITHRA B K N
+###Register Number: 212222230153
 ### Smoothing Filters
 # In[1]:Using Averaging Filter
 
@@ -8,11 +8,11 @@
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
-image1=cv2.imread("")
-image2=cv2.cvtColor(image1,cv2.COLOR_)
+image1=cv2.imread("moon.jpg")
+image2=cv2.cvtColor(image1,cv2.COLOR_BGR2RGB)
 kernel=np.ones
 image3=cv2.filter2D(image2,,kernel)
-plt.figure(figsize=())
+plt.figure(figsize=(8,8))
 plt.subplot(1,2,1)
 plt.imshow(image2)
 plt.title("Original Image")
@@ -23,10 +23,10 @@ plt.subplot()
 # In[2]:Using Weighted Averaging Filter
 
 
-kernel1=
+kernel1=np.ones((11,11), np. float32)/121
 plt.figure(figsize=(8,8))
 plt.subplot(1,)
-
+plt.imshow(image2)
 plt.title("Original Image")
 plt.axis("off")
 plt.subplot(1,2,2)
@@ -39,7 +39,7 @@ plt.show()
 # In[3]:Using Gaussian Filter
 
 
-gaussian_blur=cv2.GaussianBlur()
+gaussian_blur=cv2.GaussianBlur(src=image2, ksize=(11,11), sigmaX=0, sigmaY=0)
 plt.figure(figsize=(8,8))
 plt.subplot(1,2,1)
 plt.imshow(image2)
@@ -57,13 +57,17 @@ plt.show()
 # In[4]:Using Median Filter
 
 
-median=
+median=cv2.medianBlur (src=image2, ksize=11)
 plt.figure(figsize=(8,8))
 plt.subplot(1,2,1)
 plt.imshow(image2)
 plt.title("Original Image")
 plt.axis("off")
-plt.subplot(1,)
+plt.subplot(1,2,2)
+plt.imshow(median)
+plt.title("Median Filter")
+plt.axis("off")
+plt.show()
 
 
 
